f436a34ab346efadbca0c0f7b90ebf7e4c6049fd
lucas <lucas_alfernandes@hotmail.com>
2017-12-05 14:39:42 -0800

    New Structure

---
+++ Neural.Level/DeepNetwork.MNIST/DeepNetwork.MNIST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeepNetwork.MNIST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
